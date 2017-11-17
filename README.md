a0a0e5d067e7e1fca98a54cee0eeee93a07744a6
mir <mir@admins-imac-2.ddns.wbg.org>
2017-11-16 19:42:48 -0500

    updates for 2015

---
+++ README.md
@@ -0,0 +1,17 @@
+# ReadMe
+
+A script to retrieve documents from the [WBG document API](http://search.worldbank.org/api/v2/wds).
+
+
+  File Structure
+  --------
+
+
+      wbdocs
+      |- README                
+      |- log.txt         # Log file with the last page number and last downloaded document id.
+      |- txt/            # working directory of files being downloaded now
+      |- samples/        # Sample files
+      |  +- PolicyNotes/                          # 692 files
+      |  +- systematic-country-diagnostics/       # 210 files
+      |  +- working-papers/                       # 1735 files
--- run.py
+++ run.py
@@ -2,6 +2,14 @@ import json
 import requests
 import time
 
+
+def is_json(myjson):
+	try:
+		json.dumps(myjson)
+		return True
+	except:
+		return False
+
 num_pages = 0
 
 # World Bank public API for documents is on: http://search.worldbank.org/api/v2/wds
@@ -10,38 +18,41 @@ num_pages = 0
 # Loop in the different pages, and get the txturl field of each document
 # Save the files locally using a streaming and keep a log of the last downloaded file and page number
 document_type = "Working+Paper"
-start_date = "2016-02-01"
-end_date = "2016-05-01"
+start_date = "2015-01-01"
+end_date = "2015-12-30"
 
 url = "http://search.worldbank.org/api/v2/wds?format=json&docty_exact=" + document_type  +"&str_docdt=" + start_date + "&end_docdt=" + end_date + "&srt=docdt&order=desc"
 response = requests.get(url).json()
 num_pages = response['total']
 
 # Loop in the pages
-for page in range(8, int(num_pages)):
+for page in range(1849, int(num_pages) + 1):
 	print (str(page) + " of " + str(num_pages))
 	req = "http://search.worldbank.org/api/v2/wds?format=json&os=" + str(page) + "&docty_exact=" + document_type  +"&str_docdt=" + start_date + "&end_docdt=" + end_date + "&srt=docdt&order=desc"
-
+	# print(json.dumps(response, indent=4, sort_keys=True))
 	response = requests.get(req).json()
-	# For every item in the page, get the txturl field
-	for item in response['documents']:
-		# Test if a txturl field exists for this specific item
-		if "txturl" in response["documents"][item]:
-			# Get the url of the file to download
-			txturl = (response["documents"][item]["txturl"])
-			# Get the name of the file
-			file_name = "txt/" + txturl.split('/')[-1]
-			# Notify the user of the download progress
-			print ("Downloading " + txturl + "\n \n")
-			# Trigger the download as a stream (stream is good to use in case file is too big)
-			download = requests.get(txturl, stream=True)
-			# A workaround to ensure there is no connection reset by peer error
-			time.sleep(0.0001)
-			# Save the file in chunks of 500000 byles (~0.5 MB)
 
-			with open(file_name, 'wb') as fd:
-				for chunk in download.iter_content(500000):
-					fd.write(chunk)
+	# test if the response is a json, sometimes the json returned by API are not valid
+	if is_json(response):
+		# For every item in the page, get the txturl field
+		for item in response['documents']:
+			# Test if a txturl field exists for this specific item
+			if "txturl" in response["documents"][item]:
+				# Get the url of the file to download
+				txturl = (response["documents"][item]["txturl"])
+				# Get the name of the file
+				file_name = "txt/" + txturl.split('/')[-1]
+				# Notify the user of the download progress
+				print ("Downloading " + txturl + "\n \n")
+				# Trigger the download as a stream (stream is good to use in case file is too big)
+				download = requests.get(txturl, stream=True)
+				# A workaround to ensure there is no connection reset by peer error
+				time.sleep(0.001)
+				# Save the file in chunks of 500000 byles (~0.5 MB)
 
-			with open('log.txt', 'w') as log_file:
-				log_file.write("page: " + str(page) + ', guid: ' + response["documents"][item]["guid"])
+				with open(file_name, 'wb') as fd:
+					for chunk in download.iter_content(500000):
+						fd.write(chunk)
+
+				with open('log.txt', 'w') as log_file:
+					log_file.write("page: " + str(page) + ', guid: ' + response["documents"][item]["guid"])
