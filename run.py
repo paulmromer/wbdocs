f6456f6d42f13414d9e7d799613846172e514dae
mir <mir@admins-iMac-2.local>
2017-11-17 20:18:30 -0500

    files from 2015

--- run.py
+++ run.py
@@ -18,18 +18,20 @@ num_pages = 0
 # Loop in the different pages, and get the txturl field of each document
 # Save the files locally using a streaming and keep a log of the last downloaded file and page number
 document_type = "Working+Paper"
-start_date = "2015-01-01"
-end_date = "2015-12-30"
+start_date = "2014-01-01"
+end_date = "2015-01-01"
 
 url = "http://search.worldbank.org/api/v2/wds?format=json&docty_exact=" + document_type  +"&str_docdt=" + start_date + "&end_docdt=" + end_date + "&srt=docdt&order=desc"
 response = requests.get(url).json()
 num_pages = response['total']
 
 # Loop in the pages
-for page in range(1849, int(num_pages) + 1):
+for page in range(1863, int(num_pages) + 1):
 	print (str(page) + " of " + str(num_pages))
 	req = "http://search.worldbank.org/api/v2/wds?format=json&os=" + str(page) + "&docty_exact=" + document_type  +"&str_docdt=" + start_date + "&end_docdt=" + end_date + "&srt=docdt&order=desc"
 	# print(json.dumps(response, indent=4, sort_keys=True))
+	time.sleep(0.01)
+
 	response = requests.get(req).json()
 
 	# test if the response is a json, sometimes the json returned by API are not valid
@@ -47,7 +49,7 @@ for page in range(1849, int(num_pages) + 1):
 				# Trigger the download as a stream (stream is good to use in case file is too big)
 				download = requests.get(txturl, stream=True)
 				# A workaround to ensure there is no connection reset by peer error
-				time.sleep(0.001)
+				time.sleep(0.01)
 				# Save the file in chunks of 500000 byles (~0.5 MB)
 
 				with open(file_name, 'wb') as fd:
